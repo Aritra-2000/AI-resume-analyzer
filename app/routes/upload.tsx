23594374a01d43d4f30ea3a842bb8a17407d0c2e
Aritra <aritra.paul555@gmail.com>
2025-08-11 00:50:16 +0530

    01: add resume page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -64,8 +64,7 @@ const Upload = () => {
         data.feedback = JSON.parse(feedbackText);
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
         setStatusText('Analysis complete, redirecting...');
-        console.log(data);
-        // navigate(`/resume/${uuid}`);
+        navigate(`/resume/${uuid}`);
 
     }
 
